785844a240ab61a1dd95db22b94035590a024378
Susan Buck <susan@susanbuck.net>
2017-03-08 23:08:35 -0500

    Dynamic routing for PracticeController. To be explained in Lecture 7.

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -4,7 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PracticeController extends Controller
-{
-    //
+class PracticeController extends Controller {
+
+    /**
+	*
+	*/
+    public function practice1() {
+        dump('This is the first example.');
+    }
+
+
+    /**
+	* ANY (GET/POST/PUT/DELETE)
+    * /practice/{n?}
+    *
+    * This method accepts all requests to /practice/ and
+    * invokes the appropriate method.
+    *
+    * http://foobooks.loc/practice/1 => Invokes practice1
+    * http://foobooks.loc/practice/5 => Invokes practice5
+    * http://foobooks.loc/practice/999 => Practice route [practice999] not defined
+	*/
+    public function index($n) {
+
+        $method = 'practice'.$n;
+
+        if(method_exists($this, $method))
+            return $this->$method();
+        else
+            dd("Practice route [{$n}] not defined");
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+
+/**
+* Book related routes
+*/
+Route::get('/books', 'BookController@index');
+
 Route::get('/books', 'BookController@index');
 
 Route::get('/books/{title?}', 'BookController@view');
 
+
+
+Route::any('/practice/{n?}', 'PracticeController@index');
+
+
+
+/**
+* Main homepage visitors see when they visit just /
+*/
 Route::get('/', 'WelcomeController');
