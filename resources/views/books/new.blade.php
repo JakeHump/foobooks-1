a47572ec8ea9b42e92999114ff44075a21e8cc9c
Susan Buck <susan@susanbuck.net>
2017-04-20 02:50:08 -0400

    Reset books/new.blade.php to what it was after Lecture 11

--- resources/views/books/new.blade.php
+++ resources/views/books/new.blade.php
@@ -11,21 +11,12 @@
     <form method='POST' action='/books/new'>
         {{ csrf_field() }}
 
-        <small>* = Required field</small>
-
-        <label for='title'>* Title</label>
+        <label for='title'>Title</label>
         <input type='text' name='title' id='title' value='{{ old('title') }}'>
 
-        <label for='publishedYear'>* Published Year (YYYY)</label>
+        <label for='publishedYear'>Published Year</label>
         <input type='text' name='publishedYear' id='publishedYear' value='{{ old('publishedYear') }}'>
 
-        <label for='cover'>* URL to a cover image</label>
-        <input type='text' name='cover' id='cover' value='{{ old('cover') }}'>
-
-        <label for='purchase_link'>* Purchase link</label>
-        <input type='text' name='purchase_link' id='purchase_link' value='{{ old('purchase_link') }}'>
-
-        <br>
         <input type='submit' value='Add new book'>
     </form>
 
